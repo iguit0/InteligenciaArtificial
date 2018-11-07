2c52d5bed3aa8ef52cc1bbc03e0a59d90c9f1f9a
Igor Alves <igorsk89@gmail.com>
2018-11-07 04:46:01 -0200

    Modificações em listas

--- listas.pl
+++ listas.pl
@@ -1,4 +1,4 @@
-/* Faça os seguintes predicados (funções) em Prolog que sejam capazes de trabalhar com listas:
+﻿/* Faça os seguintes predicados (funções) em Prolog que sejam capazes de trabalhar com listas:
 • Retorne o último elemento de uma lista
 • Indique que dois elementos são consecutivos
 • Retorne o número de elementos de uma lista
@@ -8,39 +8,58 @@
 • Inverta os elementos de uma lista
 • Indique se uma lista é subconjunto de outra */
 
-pertence(Elem,[Elem|_ ]).
-pertence(Elem,[ _| Cauda]) :- pertence(Elem,Cauda).
+% Retorne o último elemento de uma lista
+mostrar_ultimo([X], [X]).
+mostrar_ultimo([Cab|Resto], R) :- mostrar_ultimo(Resto, R).
 
-insere_primeiro(X,Y,[X|Y]).
-insere_primeiro(X,[Cab|Resto],[X|[Cab|Resto]]).
+% Indique que dois elementos são consecutivos
+consecutivos([L],N).
+consecutivos([C|L],N) :- consecutivos(L,A), ( N == A -> ).
 
-% Insere um elemento no início de uma lista
-insereInicio(H, L, [H|L]):- !.
-
-inserePrimeira(X,L,[X|L]).
-
-insere(X,L, [X|L]).
-
-% numero de elementos de uma lista
+% Retorne o número de elementos de uma lista
 no_elem([],0).
 no_elem([Elem|Cauda],N) :- no_elem(Cauda,N1), N is N1+1.
 
+% Insira um elemento em qualquer posição da lista
+insereN(X,1,L,[X|L]).
+insereN(X,N,[C|L],[C|R]):-N1 is N-1, insereN(X,N1,L,R).
+
+% Retire todas as ocorrências de um elemento em uma lista
+remove_x(_,[],[]).
+remove_x(X,[X|T], L)      :- remove_x(X,T,L).
+remove_x(X,[C|T], [C|T1]) :- X \= C, remove_x(X,T,T1).
+
+%Indique se a lista possui tamanho par ou ímpar
+% ...
+
+% Inverta os elementos de uma lista
+% ...
+
+%Indique se uma lista é subconjunto de outra
+% ...
+
+% Questao 6 - segunda lista
+% Verificar se um elemento pertence alguma lista
+pertence(Elem,[Elem|_ ]).
+pertence(Elem,[ _| Cauda]) :- pertence(Elem,Cauda).
+
+% Insere um elemento no início de uma lista
+insere_primeiro(X,Y,[X|Y]).
+insere_primeiro(X,[Cab|Resto],[X|[Cab|Resto]]).
+
+insereInicio(H, L, [H|L]):- !.
+inserePrimeira(X,L,[X|L]).
+insere(X,L, [X|L]).
+
+
+% Retirar Elemento
 retirar_elemento(Elem,[Elem|Cauda],Cauda).
-retirar_elemento(Elem,[Cabeça|Cauda],[Cabeça|Resultado]) :- retirar_elemento(Elem,Cauda,Resultado).
+retirar_elemento(Elem,[Cab|Cauda],[Cab|Resultado]) :- retirar_elemento(Elem,Cauda,Resultado).
 
 concatena([ ],L,L).
 concatena([Cab|Cauda],L2,[Cab|Resultado]) :- concatena(Cauda,L2,Resultado).
-
-
-
-mostrar_ultimo([X], [X]).
-mostrar_ultimo([Cab|Resto], R) :- mostrar_ultimo(Resto, R).
 
 inserir_final([], Y, [Y]).         % Se a lista estava vazia, o resultado é [Y]
 inserir_final([I|R], Y, [I|R1]) :- % Senão, o primeiro elemento é igual, e o resto é obtido
     inserir_final(R, Y, R1).       % Inserindo o elemento Y no final do antigo resto
 
-
-% Verificar se dois elementos sao consecutivos em uma lista.
-consecutivos([L],N).
-consecutivos([C|L],N) :- consecutivos(L,A), ( N == A -> ).
